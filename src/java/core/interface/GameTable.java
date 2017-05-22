0d2e3d012e5df4e2a41a2a7e3057bf517454965b
HQ Liu <hqliu.tech@gmail.com>
2017-05-21 23:13:46 -0700

    re-define GameTable

--- src/java/core/interface/GameTable.java
+++ src/java/core/interface/GameTable.java
@@ -1,3 +1,19 @@
 public interface GameTable {
-	public boolean addPlayer(Player player);
+	public int size();
+	
+	public boolean isFull();
+
+	public boolean isEmpty();
+
+	public boolean isStarted();
+
+	public boolean isEnded();
+
+	public boolean start();
+
+	public boolean terminate();
+
+	public GameStatus getStatus();
+
+	public GameStatus getPlayerStatus(Player p);
 }
